16096c6535564f333b459f3dec22ec1739d42c99
ChaitanyaAkula <krishchaituakula@gmail.com>
2020-05-18 19:03:16 +0530

    Update dbconnection.go

--- dbconnection.go
+++ dbconnection.go
@@ -8,7 +8,7 @@ import(
 var db *sql.DB
 func Connection()*sql.DB{
 
-	Db,err := sql.Open("mysql","root:root@tcp(localhost:3306)/gittyjobs")
+	Db,err := sql.Open("mysql","gittysourcing:Ancons927@tcp(gittysourcing.crp5gpwzu2zf.us-east-2.rds.amazonaws.com:3306)/gittyjobs")
 	
 	if err!=nil{
 		log.Fatalln(err)
